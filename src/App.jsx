fd182c56a622d10546f1e700644d3b4172dbdec1
cuckoodile <lhourdeiansube7@gmail.com>
2024-11-07 13:07:19 +0800

    updated web

--- src/App.jsx
+++ src/App.jsx
@@ -18,12 +18,20 @@ import {
 } from "@/components/ui/carousel";
 
 function App() {
+  
   return (
-    <section className="flex flex-col flex-1 min-h-0 ">
+    <section className="dark bg-background text-foreground flex flex-col flex-1 min-h-0 ">
       <nav>
-        <h1>hello</h1>
+        <ul className='text-2xl flex gap-5 fixed z-10 w-full bg-[#717274B3] justify-end px-5 py-2'>
+          <li><a href="#home">Home</a></li>
+          <li><a href="">About</a></li>
+          <li><a href="">Cart</a></li>
+          <li><a href="">Log Out</a></li>
+        </ul>
       </nav>
-      <div className="w-full h-[300px] px-20">
+      <span id="home">{/* For home navigation only*/}</span>
+      <div className="w-full h-[300px] px-20 mt-20">
+        {/* Advertisement banner */}
         <Carousel>
           <CarouselContent>
             <CarouselItem>
@@ -51,32 +59,371 @@ function App() {
           <CarouselPrevious />
           <CarouselNext />
         </Carousel>
-        <b>SKULL PANDA THE SOUND SERIES</b>
+
+        {/* Products */}
       </div>
-      <section className="flex  gap-5 p-5 flex-1 min-h-0 flex-wrap ">
-        <Card className="w-[350px] h-[520px] bg-zinc-800">
-          <CardHeader>
-            <CardTitle>
-              <img
-                src="https://u-mercari-images.mercdn.net/photos/m29617180333_1.jpg?width=512&height=512&format=pjpg&auto=webp&fit=crop&_=1720234075"
-                className="w-full h-[300px]"
-                alt="TRY"
-              ></img>
-            </CardTitle>
-          </CardHeader>
-          <CardContent className="text-white">
-            <p>The Vigilance</p>
-            <p>PHP 2000.00</p>
-            <p>
-              The silver-blue part of magnetic mask has a metallic pearlescent
-              effect
-            </p>
-          </CardContent>
-          <CardFooter>
-            <Button>Add to Cart</Button>
-          </CardFooter>
-        </Card>
-      </section>
+      <div className="mt-10 flex flex-col p-5 gap-5">
+        <b className="text-4xl text-center">SKULL PANDA THE SOUND SERIES</b>
+        <section className="flex gap-5 flex-1 min-h-0 flex-wrap justify-center">
+          <Card className="w-[350px] h-[520px] bg-zinc-800">
+            <CardHeader>
+              <CardTitle>
+                <img
+                  src="https://u-mercari-images.mercdn.net/photos/m29617180333_1.jpg?width=512&height=512&format=pjpg&auto=webp&fit=crop&_=1720234075"
+                  className="w-full h-[300px]"
+                  alt="TRY"
+                ></img>
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="text-white">
+              <p>The Vigilance</p>
+              <p>PHP 2000.00</p>
+              <p>
+                The silver-blue part of magnetic mask has a metallic pearlescent
+                effect
+              </p>
+            </CardContent>
+            <CardFooter>
+              <Button>Add to Cart</Button>
+            </CardFooter>
+          </Card>
+          <Card className="w-[350px] h-[520px] bg-zinc-800">
+            <CardHeader>
+              <CardTitle>
+                <img
+                  src="https://u-mercari-images.mercdn.net/photos/m29617180333_1.jpg?width=512&height=512&format=pjpg&auto=webp&fit=crop&_=1720234075"
+                  className="w-full h-[300px]"
+                  alt="TRY"
+                ></img>
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="text-white">
+              <p>The Vigilance</p>
+              <p>PHP 2000.00</p>
+              <p>
+                The silver-blue part of magnetic mask has a metallic pearlescent
+                effect
+              </p>
+            </CardContent>
+            <CardFooter>
+              <Button>Add to Cart</Button>
+            </CardFooter>
+          </Card>
+          <Card className="w-[350px] h-[520px] bg-zinc-800">
+            <CardHeader>
+              <CardTitle>
+                <img
+                  src="https://u-mercari-images.mercdn.net/photos/m29617180333_1.jpg?width=512&height=512&format=pjpg&auto=webp&fit=crop&_=1720234075"
+                  className="w-full h-[300px]"
+                  alt="TRY"
+                ></img>
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="text-white">
+              <p>The Vigilance</p>
+              <p>PHP 2000.00</p>
+              <p>
+                The silver-blue part of magnetic mask has a metallic pearlescent
+                effect
+              </p>
+            </CardContent>
+            <CardFooter>
+              <Button>Add to Cart</Button>
+            </CardFooter>
+          </Card>
+          <Card className="w-[350px] h-[520px] bg-zinc-800">
+            <CardHeader>
+              <CardTitle>
+                <img
+                  src="https://u-mercari-images.mercdn.net/photos/m29617180333_1.jpg?width=512&height=512&format=pjpg&auto=webp&fit=crop&_=1720234075"
+                  className="w-full h-[300px]"
+                  alt="TRY"
+                ></img>
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="text-white">
+              <p>The Vigilance</p>
+              <p>PHP 2000.00</p>
+              <p>
+                The silver-blue part of magnetic mask has a metallic pearlescent
+                effect
+              </p>
+            </CardContent>
+            <CardFooter>
+              <Button>Add to Cart</Button>
+            </CardFooter>
+          </Card>
+          <Card className="w-[350px] h-[520px] bg-zinc-800">
+            <CardHeader>
+              <CardTitle>
+                <img
+                  src="https://u-mercari-images.mercdn.net/photos/m29617180333_1.jpg?width=512&height=512&format=pjpg&auto=webp&fit=crop&_=1720234075"
+                  className="w-full h-[300px]"
+                  alt="TRY"
+                ></img>
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="text-white">
+              <p>The Vigilance</p>
+              <p>PHP 2000.00</p>
+              <p>
+                The silver-blue part of magnetic mask has a metallic pearlescent
+                effect
+              </p>
+            </CardContent>
+            <CardFooter>
+              <Button>Add to Cart</Button>
+            </CardFooter>
+          </Card>
+          <Card className="w-[350px] h-[520px] bg-zinc-800">
+            <CardHeader>
+              <CardTitle>
+                <img
+                  src="https://u-mercari-images.mercdn.net/photos/m29617180333_1.jpg?width=512&height=512&format=pjpg&auto=webp&fit=crop&_=1720234075"
+                  className="w-full h-[300px]"
+                  alt="TRY"
+                ></img>
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="text-white">
+              <p>The Vigilance</p>
+              <p>PHP 2000.00</p>
+              <p>
+                The silver-blue part of magnetic mask has a metallic pearlescent
+                effect
+              </p>
+            </CardContent>
+            <CardFooter>
+              <Button>Add to Cart</Button>
+            </CardFooter>
+          </Card>
+          <Card className="w-[350px] h-[520px] bg-zinc-800">
+            <CardHeader>
+              <CardTitle>
+                <img
+                  src="https://u-mercari-images.mercdn.net/photos/m29617180333_1.jpg?width=512&height=512&format=pjpg&auto=webp&fit=crop&_=1720234075"
+                  className="w-full h-[300px]"
+                  alt="TRY"
+                ></img>
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="text-white">
+              <p>The Vigilance</p>
+              <p>PHP 2000.00</p>
+              <p>
+                The silver-blue part of magnetic mask has a metallic pearlescent
+                effect
+              </p>
+            </CardContent>
+            <CardFooter>
+              <Button>Add to Cart</Button>
+            </CardFooter>
+          </Card>
+          <Card className="w-[350px] h-[520px] bg-zinc-800">
+            <CardHeader>
+              <CardTitle>
+                <img
+                  src="https://u-mercari-images.mercdn.net/photos/m29617180333_1.jpg?width=512&height=512&format=pjpg&auto=webp&fit=crop&_=1720234075"
+                  className="w-full h-[300px]"
+                  alt="TRY"
+                ></img>
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="text-white">
+              <p>The Vigilance</p>
+              <p>PHP 2000.00</p>
+              <p>
+                The silver-blue part of magnetic mask has a metallic pearlescent
+                effect
+              </p>
+            </CardContent>
+            <CardFooter>
+              <Button>Add to Cart</Button>
+            </CardFooter>
+          </Card>
+        </section>
+      </div>
+      <div className="mt-10 flex flex-col p-5 gap-5">
+        <b className="text-4xl text-center">POWER PUFF SERIES</b>
+        <section className="flex gap-5 flex-1 min-h-0 flex-wrap justify-center">
+          <Card className="w-[350px] h-[520px] bg-zinc-800">
+            <CardHeader>
+              <CardTitle>
+                <img
+                  src="https://u-mercari-images.mercdn.net/photos/m29617180333_1.jpg?width=512&height=512&format=pjpg&auto=webp&fit=crop&_=1720234075"
+                  className="w-full h-[300px]"
+                  alt="TRY"
+                ></img>
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="text-white">
+              <p>The Vigilance</p>
+              <p>PHP 2000.00</p>
+              <p>
+                The silver-blue part of magnetic mask has a metallic pearlescent
+                effect
+              </p>
+            </CardContent>
+            <CardFooter>
+              <Button>Add to Cart</Button>
+            </CardFooter>
+          </Card>
+          <Card className="w-[350px] h-[520px] bg-zinc-800">
+            <CardHeader>
+              <CardTitle>
+                <img
+                  src="https://u-mercari-images.mercdn.net/photos/m29617180333_1.jpg?width=512&height=512&format=pjpg&auto=webp&fit=crop&_=1720234075"
+                  className="w-full h-[300px]"
+                  alt="TRY"
+                ></img>
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="text-white">
+              <p>The Vigilance</p>
+              <p>PHP 2000.00</p>
+              <p>
+                The silver-blue part of magnetic mask has a metallic pearlescent
+                effect
+              </p>
+            </CardContent>
+            <CardFooter>
+              <Button>Add to Cart</Button>
+            </CardFooter>
+          </Card>
+          <Card className="w-[350px] h-[520px] bg-zinc-800">
+            <CardHeader>
+              <CardTitle>
+                <img
+                  src="https://u-mercari-images.mercdn.net/photos/m29617180333_1.jpg?width=512&height=512&format=pjpg&auto=webp&fit=crop&_=1720234075"
+                  className="w-full h-[300px]"
+                  alt="TRY"
+                ></img>
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="text-white">
+              <p>The Vigilance</p>
+              <p>PHP 2000.00</p>
+              <p>
+                The silver-blue part of magnetic mask has a metallic pearlescent
+                effect
+              </p>
+            </CardContent>
+            <CardFooter>
+              <Button>Add to Cart</Button>
+            </CardFooter>
+          </Card>
+          <Card className="w-[350px] h-[520px] bg-zinc-800">
+            <CardHeader>
+              <CardTitle>
+                <img
+                  src="https://u-mercari-images.mercdn.net/photos/m29617180333_1.jpg?width=512&height=512&format=pjpg&auto=webp&fit=crop&_=1720234075"
+                  className="w-full h-[300px]"
+                  alt="TRY"
+                ></img>
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="text-white">
+              <p>The Vigilance</p>
+              <p>PHP 2000.00</p>
+              <p>
+                The silver-blue part of magnetic mask has a metallic pearlescent
+                effect
+              </p>
+            </CardContent>
+            <CardFooter>
+              <Button>Add to Cart</Button>
+            </CardFooter>
+          </Card>
+          <Card className="w-[350px] h-[520px] bg-zinc-800">
+            <CardHeader>
+              <CardTitle>
+                <img
+                  src="https://u-mercari-images.mercdn.net/photos/m29617180333_1.jpg?width=512&height=512&format=pjpg&auto=webp&fit=crop&_=1720234075"
+                  className="w-full h-[300px]"
+                  alt="TRY"
+                ></img>
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="text-white">
+              <p>The Vigilance</p>
+              <p>PHP 2000.00</p>
+              <p>
+                The silver-blue part of magnetic mask has a metallic pearlescent
+                effect
+              </p>
+            </CardContent>
+            <CardFooter>
+              <Button>Add to Cart</Button>
+            </CardFooter>
+          </Card>
+          <Card className="w-[350px] h-[520px] bg-zinc-800">
+            <CardHeader>
+              <CardTitle>
+                <img
+                  src="https://u-mercari-images.mercdn.net/photos/m29617180333_1.jpg?width=512&height=512&format=pjpg&auto=webp&fit=crop&_=1720234075"
+                  className="w-full h-[300px]"
+                  alt="TRY"
+                ></img>
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="text-white">
+              <p>The Vigilance</p>
+              <p>PHP 2000.00</p>
+              <p>
+                The silver-blue part of magnetic mask has a metallic pearlescent
+                effect
+              </p>
+            </CardContent>
+            <CardFooter>
+              <Button>Add to Cart</Button>
+            </CardFooter>
+          </Card>
+          <Card className="w-[350px] h-[520px] bg-zinc-800">
+            <CardHeader>
+              <CardTitle>
+                <img
+                  src="https://u-mercari-images.mercdn.net/photos/m29617180333_1.jpg?width=512&height=512&format=pjpg&auto=webp&fit=crop&_=1720234075"
+                  className="w-full h-[300px]"
+                  alt="TRY"
+                ></img>
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="text-white">
+              <p>The Vigilance</p>
+              <p>PHP 2000.00</p>
+              <p>
+                The silver-blue part of magnetic mask has a metallic pearlescent
+                effect
+              </p>
+            </CardContent>
+            <CardFooter>
+              <Button>Add to Cart</Button>
+            </CardFooter>
+          </Card>
+          <Card className="w-[350px] h-[520px] bg-zinc-800">
+            <CardHeader>
+              <CardTitle>
+                <img
+                  src="https://u-mercari-images.mercdn.net/photos/m29617180333_1.jpg?width=512&height=512&format=pjpg&auto=webp&fit=crop&_=1720234075"
+                  className="w-full h-[300px]"
+                  alt="TRY"
+                ></img>
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="text-white">
+              <p>The Vigilance</p>
+              <p>PHP 2000.00</p>
+              <p>
+                The silver-blue part of magnetic mask has a metallic pearlescent
+                effect
+              </p>
+            </CardContent>
+            <CardFooter>
+              <Button>Add to Cart</Button>
+            </CardFooter>
+          </Card>
+        </section>
+      </div>
     </section>
   );
 }
